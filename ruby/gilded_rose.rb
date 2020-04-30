f2bc27fbfcd29bec5cfa3eaaba32631ea5611af8
Minghua Zhao <minghz42@gmail.com>
2020-04-29 22:04:16 -0400

    Extract normal items

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -14,20 +14,7 @@ class GildedRose
       elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
         BackstagePass.age(item)
       else
-
-        if item.quality > 0
-          item.quality = item.quality - 1
-        end
-
-        item.sell_in = item.sell_in - 1
-
-        if item.sell_in < 0
-
-          if item.quality > 0
-            item.quality = item.quality - 1
-          end
-
-        end
+        NormalItem.age(item)
       end
     end
   end
@@ -44,6 +31,20 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+module NormalItem
+  def self.age(item)
+    if item.sell_in <= 0
+      item.quality = item.quality - 2
+    else
+      item.quality = item.quality - 1
+    end
+
+    item.quality = 0 if item.quality < 0
+
+    item.sell_in = item.sell_in - 1
   end
 end
 
